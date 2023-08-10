5c28009852fb591171552ab9c4babcacc6af9ad3
Guilherme Sousa <guilhermenoobsaibot@gmail.com>
2023-08-10 09:25:48 -0300

    Init adding test e2e on project

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import fastify from 'fastify';
 import { ZodError } from 'zod';
 
-import { appRoutes } from './http/routes';
+import { usersRoutes } from './http/controllers/users/routes';
+import { gymsRoutes } from './http/controllers/gyms/routes';
 import { env } from './env';
 import fastifyJwt from '@fastify/jwt';
 
@@ -11,7 +12,8 @@ app.register(fastifyJwt, {
 	secret: env.JWT_SECRET
 });
 
-app.register(appRoutes);
+app.register(usersRoutes);
+app.register(gymsRoutes);
 
 app.setErrorHandler((error, _, reply) => {
 	if (error instanceof ZodError) {
